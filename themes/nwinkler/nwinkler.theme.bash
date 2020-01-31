7f518259a73bf267ccb945b304d02725e13b3b93
Jeff Fisher <jeff.fisher@hpbsc.ch>
2020-01-31 12:07:55 +0100

    nwinkler theme breaks history :(

--- themes/nwinkler/nwinkler.theme.bash
+++ themes/nwinkler/nwinkler.theme.bash
@@ -28,10 +28,11 @@ prompt_setter() {
   if [[ $exit_status -eq 0 ]]; then PROMPT_END=$PROMPT_END_CLEAN
     else PROMPT_END=$PROMPT_END_DIRTY
   fi
-  # Save history
-  history -a
-  history -c
-  history -r
+  # # Save history
+  # This actually breaks history :-(
+  # history -a
+  # history -c
+  # history -r
   PS1="($(clock_prompt)) $(scm_char) [${blue}\u${reset_color}@${green}\H${reset_color}] ${yellow}\w${reset_color}$(scm_prompt_info) ${reset_color}\n$(prompt_end) "
   PS2='> '
   PS4='+ '
