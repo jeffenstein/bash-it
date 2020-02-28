fee33db4fdda524c2943552630b273fb6287cfaa
Jeff Fisher <jeff.fisher@hpbsc.ch>
2020-02-28 15:10:46 +0100

    Custom theme based on powerline

---
+++ custom/themes/jeff/jeff.theme.bash
@@ -0,0 +1,303 @@
+#!/usr/bin/env bash
+
+for __n in git hg svn
+do
+    if [[ -n "$(command which "$__n" 2>/dev/null)" ]];
+    then
+        eval "__${__n}=yes"
+    else
+        eval "__${__n}=no"
+    fi
+done
+unset __n
+
+function __c {
+    local code="${1:-}"
+    local string="${2:-}"
+    local __l="⁅"
+    local __r="⁆"
+    if [[ -n "$code" && -n "$string" ]]; then
+        echo "\[${code}\]${__l}${string}${__r}\[${term_normal}\]"
+    fi
+}
+
+function __my_hostname_prompt {
+    if [[ -z "${HOST:-}" ]]; then
+        HOST="$(uname -n | cut -d. -f1)"
+    fi
+    __c "${fg_green}" "$HOST${HOST_FRIENDLY:+(${HOST_FRIENDLY})}"
+}
+
+function __my_clock_prompt {
+    __c "${fg_yellow}" "$(date '+%H:%M:%S')"
+}
+
+function __my_username_prompt {
+    if [[ -z "${USER:-}" ]];then
+        USER="$(id -u 2>/dev/null)"
+    fi
+    if [[ -z "${USER}" ]]; then
+        USER="unknown"
+    fi
+    __c "${fg_cyan}" "${USER}"
+}
+
+function __my_last_status_prompt {
+    local txt="rval:$1"
+    if [[ "$1" -ne 0 ]]; then
+        __c "${fg_red}" "$txt"
+    # else
+    #     __c "${fg_cyan}" "$txt"
+    fi
+}
+
+function __my_pwd_prompt {
+    local newpwd="${PWD#${HOME}}"
+    if [[ "$newpwd" != "${PWD}" ]]; then
+        newpwd="~${newpwd}"
+    fi
+    __c "${fg_green}" "$newpwd"
+}
+
+function __my_prompt_command {
+  local last_status="$?" ## always the first
+  local prompt=''
+  local __l="⁅"
+  local __r="⁆"
+  local __rl="${__r}${__l}"
+
+  # [[ "${last_status}" -ne 0 ]] && __powerline_left_segment $(__powerline_last_status_prompt ${last_status})
+  # fix pwd... shorten ~ and use __c with fg_green
+  PS1="$(__my_username_prompt)$(__my_hostname_prompt)$(__my_clock_prompt)$(__my_last_status_prompt "${last_status}")
+$(__my_pwd_prompt)
+\$ "
+  # LEFT_PROMPT=""
+  # SEGMENTS_AT_LEFT=0
+  # LAST_SEGMENT_COLOR=""
+
+
+  # if [[ -n "${POWERLINE_PROMPT_DISTRO_LOGO}" ]]; then
+  #     LEFT_PROMPT+="$(set_color ${PROMPT_DISTRO_LOGO_COLOR} ${PROMPT_DISTRO_LOGO_COLORBG})${PROMPT_DISTRO_LOGO}$(set_color - -)"
+  # fi
+
+  # ## left prompt ##
+  # for segment in $POWERLINE_PROMPT; do
+  #   local info="$(__powerline_${segment}_prompt)"
+  #   [[ -n "${info}" ]] && __powerline_left_segment "${info}"
+  # done
+
+  # [[ "${last_status}" -ne 0 ]] && __powerline_left_segment $(__powerline_last_status_prompt ${last_status})
+  # [[ -n "${LEFT_PROMPT}" ]] && LEFT_PROMPT+="$(set_color ${LAST_SEGMENT_COLOR} -)${separator_char}${normal}"
+
+
+  # ## cleanup ##
+  # unset LAST_SEGMENT_COLOR \
+  #       LEFT_PROMPT \
+  #       SEGMENTS_AT_LEFT
+
+}
+
+# # Define this here so it can be used by all of the Powerline themes
+# THEME_CHECK_SUDO=${THEME_CHECK_SUDO:=true}
+
+# function set_color {
+#   set +u
+#   if [[ "${1}" != "-" ]]; then
+#     fg="38;5;${1}"
+#   fi
+#   if [[ "${2}" != "-" ]]; then
+#     bg="48;5;${2}"
+#     [[ -n "${fg}" ]] && bg=";${bg}"
+#   fi
+#   echo -e "\[\033[${fg}${bg}m\]"
+# }
+
+# function __powerline_user_info_prompt {
+#   local user_info=""
+#   local color=${USER_INFO_THEME_PROMPT_COLOR}
+
+#   if [[ "${THEME_CHECK_SUDO}" = true ]]; then
+#     if sudo -n uptime 2>&1 | grep -q "load"; then
+#       color=${USER_INFO_THEME_PROMPT_COLOR_SUDO}
+#     fi
+#   fi
+
+#   case "${POWERLINE_PROMPT_USER_INFO_MODE}" in
+#     "sudo")
+#       if [[ "${color}" = "${USER_INFO_THEME_PROMPT_COLOR_SUDO}" ]]; then
+#         user_info="!"
+#       fi
+#       ;;
+#     *)
+#       if [[ -n "${SSH_CLIENT}" ]] || [[ -n "${SSH_CONNECTION}" ]]; then
+#         user_info="${USER_INFO_SSH_CHAR}${USER}"
+#       else
+#         user_info="${USER}"
+#       fi
+#       ;;
+#   esac
+#   [[ -n "${user_info}" ]] && echo "${user_info}|${color}"
+# }
+
+# function __powerline_node_prompt {
+#   local node_version=""
+
+#   node_version="$(node_version_prompt)"
+#   [[ -n "${node_version}" ]] && echo "${NODE_CHAR}${node_version}|${NODE_THEME_PROMPT_COLOR}"
+# }
+
+# function __powerline_ruby_prompt {
+#   local ruby_version=""
+
+#   if _command_exists rvm; then
+#     ruby_version="$(rvm_version_prompt)"
+#   elif _command_exists rbenv; then
+#     ruby_version=$(rbenv_version_prompt)
+#   fi
+
+#   [[ -n "${ruby_version}" ]] && echo "${RUBY_CHAR}${ruby_version}|${RUBY_THEME_PROMPT_COLOR}"
+# }
+
+# function __powerline_k8s_context_prompt {
+#   local kubernetes_context=""
+
+#   if _command_exists kubectl; then
+#     kubernetes_context="$(k8s_context_prompt)"
+#   fi
+
+#   [[ -n "${kubernetes_context}" ]] && echo "${KUBERNETES_CONTEXT_THEME_CHAR}${kubernetes_context}|${KUBERNETES_CONTEXT_THEME_PROMPT_COLOR}"
+# }
+
+# function __powerline_python_venv_prompt {
+#   set +u
+#   local python_venv=""
+
+#   if [[ -n "${CONDA_DEFAULT_ENV}" ]]; then
+#     python_venv="${CONDA_DEFAULT_ENV}"
+#     PYTHON_VENV_CHAR=${CONDA_PYTHON_VENV_CHAR}
+#   elif [[ -n "${VIRTUAL_ENV}" ]]; then
+#     python_venv=$(basename "${VIRTUAL_ENV}")
+#   fi
+
+#   [[ -n "${python_venv}" ]] && echo "${PYTHON_VENV_CHAR}${python_venv}|${PYTHON_VENV_THEME_PROMPT_COLOR}"
+# }
+
+# function __powerline_scm_prompt {
+#   local color=""
+#   local scm_prompt=""
+
+#   scm_prompt_vars
+
+#   if [[ "${SCM_NONE_CHAR}" != "${SCM_CHAR}" ]]; then
+#     if [[ "${SCM_DIRTY}" -eq 3 ]]; then
+#       color=${SCM_THEME_PROMPT_STAGED_COLOR}
+#     elif [[ "${SCM_DIRTY}" -eq 2 ]]; then
+#       color=${SCM_THEME_PROMPT_UNSTAGED_COLOR}
+#     elif [[ "${SCM_DIRTY}" -eq 1 ]]; then
+#       color=${SCM_THEME_PROMPT_DIRTY_COLOR}
+#     else
+#       color=${SCM_THEME_PROMPT_CLEAN_COLOR}
+#     fi
+#     if [[ "${SCM_GIT_CHAR}" == "${SCM_CHAR}" ]]; then
+#       scm_prompt+="${SCM_CHAR}${SCM_BRANCH}${SCM_STATE}"
+#     elif [[ "${SCM_P4_CHAR}" == "${SCM_CHAR}" ]]; then
+#       scm_prompt+="${SCM_CHAR}${SCM_BRANCH}${SCM_STATE}"
+#     elif [[ "${SCM_HG_CHAR}" == "${SCM_CHAR}" ]]; then
+#       scm_prompt+="${SCM_CHAR}${SCM_BRANCH}${SCM_STATE}"
+#     elif [[ "${SCM_SVN_CHAR}" == "${SCM_CHAR}" ]]; then
+#       scm_prompt+="${SCM_CHAR}${SCM_BRANCH}${SCM_STATE}"
+#     fi
+#     echo "$(eval "echo ${scm_prompt}")${scm}|${color}"
+#   fi
+# }
+
+# function __powerline_cwd_prompt {
+#   local cwd=$(pwd | sed "s|^${HOME}|~|")
+
+#   echo "${cwd}|${CWD_THEME_PROMPT_COLOR}"
+# }
+
+
+# function __powerline_wd_prompt {
+#   echo "\W|${CWD_THEME_PROMPT_COLOR}"
+# }
+
+
+# function __powerline_battery_prompt {
+#   local color=""
+#   local battery_status="$(battery_percentage 2> /dev/null)"
+
+#   if [[ -z "${battery_status}" ]] || [[ "${battery_status}" = "-1" ]] || [[ "${battery_status}" = "no" ]]; then
+#     true
+#   else
+#     if [[ "$((10#${battery_status}))" -le 5 ]]; then
+#       color="${BATTERY_STATUS_THEME_PROMPT_CRITICAL_COLOR}"
+#     elif [[ "$((10#${battery_status}))" -le 25 ]]; then
+#       color="${BATTERY_STATUS_THEME_PROMPT_LOW_COLOR}"
+#     else
+#       color="${BATTERY_STATUS_THEME_PROMPT_GOOD_COLOR}"
+#     fi
+#     ac_adapter_connected && battery_status="${BATTERY_AC_CHAR}${battery_status}"
+#     echo "${battery_status}%|${color}"
+#   fi
+# }
+
+# function __powerline_in_vim_prompt {
+#   if [ -n "$VIMRUNTIME" ]; then
+#     echo "${IN_VIM_THEME_PROMPT_TEXT}|${IN_VIM_THEME_PROMPT_COLOR}"
+#   fi
+# }
+
+# function __powerline_aws_profile_prompt {
+#   if [[ -n "${AWS_PROFILE}" ]]; then
+#     echo "${AWS_PROFILE_CHAR}${AWS_PROFILE}|${AWS_PROFILE_PROMPT_COLOR}"
+#   fi
+# }
+
+# function __powerline_left_segment {
+#   local OLD_IFS="${IFS}"; IFS="|"
+#   local params=( $1 )
+#   IFS="${OLD_IFS}"
+#   local separator_char="${POWERLINE_LEFT_SEPARATOR}"
+#   local separator=""
+
+#   if [[ "${SEGMENTS_AT_LEFT}" -gt 0 ]]; then
+#     separator="$(set_color ${LAST_SEGMENT_COLOR} ${params[1]})${separator_char}${normal}"
+#   fi
+#   LEFT_PROMPT+="${separator}$(set_color - ${params[1]}) ${params[0]} ${normal}"
+#   LAST_SEGMENT_COLOR=${params[1]}
+#   (( SEGMENTS_AT_LEFT += 1 ))
+# }
+
+
+# function __powerline_prompt_command {
+#   local last_status="$?" ## always the first
+#   local separator_char="${POWERLINE_PROMPT_CHAR}"
+
+#   LEFT_PROMPT=""
+#   SEGMENTS_AT_LEFT=0
+#   LAST_SEGMENT_COLOR=""
+
+
+#   if [[ -n "${POWERLINE_PROMPT_DISTRO_LOGO}" ]]; then
+#       LEFT_PROMPT+="$(set_color ${PROMPT_DISTRO_LOGO_COLOR} ${PROMPT_DISTRO_LOGO_COLORBG})${PROMPT_DISTRO_LOGO}$(set_color - -)"
+#   fi
+
+#   ## left prompt ##
+#   for segment in $POWERLINE_PROMPT; do
+#     local info="$(__powerline_${segment}_prompt)"
+#     [[ -n "${info}" ]] && __powerline_left_segment "${info}"
+#   done
+
+#   [[ "${last_status}" -ne 0 ]] && __powerline_left_segment $(__powerline_last_status_prompt ${last_status})
+#   [[ -n "${LEFT_PROMPT}" ]] && LEFT_PROMPT+="$(set_color ${LAST_SEGMENT_COLOR} -)${separator_char}${normal}"
+
+#   PS1="${LEFT_PROMPT} "
+
+#   ## cleanup ##
+#   unset LAST_SEGMENT_COLOR \
+#         LEFT_PROMPT \
+#         SEGMENTS_AT_LEFT
+# }
+
+PROMPT_COMMAND=__my_prompt_command
